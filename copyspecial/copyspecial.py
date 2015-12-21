ef180256a997692f6dd29c2d1c7811be6f09aed9
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-12-21 15:24:40 -0200

    Resolved copyspecial.py

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -11,9 +11,9 @@
 
 
 import sys
-import re
 import os
 import shutil
+import shlex
 import subprocess
 
 """Copy Special exercise
@@ -21,8 +21,50 @@ import subprocess
 """
 
 
-# +++your code here+++
-# Write functions and modify main() to call them
+def get_special_paths(dir, show_output=True):
+    """
+    returns a list of the absolute paths of the
+    special files in the given directory
+    """
+    dirpath = os.path.abspath(dir)
+    files = os.listdir(dirpath)
+    allfiles = []
+    for f in files:
+        fullfile = "{}/{}".format(dirpath, f)
+        if show_output:
+            print(fullfile)
+        else:
+            allfiles.append(fullfile)
+    return allfiles
+
+
+def copy_to(paths, dir):
+    """
+    given a list of paths, copies those files into the given directory
+    """
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    for path in paths:
+        if os.path.isfile(path):
+            name = os.path.basename(path)
+            shutil.copy2(path, "{}/{}".format(dir, name))
+
+
+def zip_to(paths, zippath):
+    """
+    given a list of paths, zip those files up into the given zipfile
+    """
+
+    command = "zip -j {} ".format(zippath)
+    for path in paths:
+        if os.path.isfile(path):
+            command += " {} ".format(path)
+
+    args = shlex.split(command)
+    print("Command I'm going to do: \n\n{}".format(command))
+    print("\nOutput:\n")
+    subprocess.Popen(args)
 
 
 def main():
@@ -52,10 +94,18 @@ def main():
     if len(args) == 0:
         print("error: must specify one or more dirs")
         sys.exit(1)
-
-        # +++your code here+++
-        # Call your functions
-
+    elif len(args) > 0 and not tozip and not todir:
+        for arg in args:
+            get_special_paths(arg)
+    elif todir:
+        for arg in args:
+            files = get_special_paths(arg, show_output=False)
+            copy_to(files, todir)
+    elif tozip:
+        allfiles = []
+        for arg in args:
+            allfiles += get_special_paths(arg, show_output=False)
+        zip_to(allfiles, tozip)
 
 if __name__ == "__main__":
     main()
